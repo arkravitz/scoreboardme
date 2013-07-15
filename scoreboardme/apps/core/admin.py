71e76cd9e1093ea4f9026bee5c976a54ba357716
Aaron Kravitz <arkravitz@gmail.com>
2013-07-14 18:03:41 -0700

    still working

--- scoreboardme/apps/core/admin.py
+++ scoreboardme/apps/core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
+from django_extensions.admin import ForeignKeyAutocompleteAdmin
+
 from .models import UserProfile, Event, Score
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(ForeignKeyAutocompleteAdmin):
+    related_search_fields = {
+        'user': ('username', 'email',),
+    }
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Event)
 admin.site.register(Score)
--- scoreboardme/settings/dev.py
+++ scoreboardme/settings/dev.py
@@ -56,6 +56,7 @@ CELERY_ALWAYS_EAGER = True
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
     'debug_toolbar',
+    'django_extensions',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
@@ -65,4 +66,8 @@ INTERNAL_IPS = ('127.0.0.1',)
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
 ########## END TOOLBAR CONFIGURATION
